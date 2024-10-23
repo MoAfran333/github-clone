f9397f9130414676063f68efd89ea8c69791fb6c
MoAfran333 <moafran123@gmail.com>
2024-10-23 16:38:56 +0530

    Updated package.json file and got it ready for deployment

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import passport from "passport";
 import session from "express-session";
+import path from "path";
 
 import "./passport/github.auth.js";
 import userRoutes from "./routes/user.routes.js";
@@ -13,6 +14,9 @@ import connectDB from "./config/connectionMongoDB.js";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+const buildPath = path.join(__dirname, "../client");
 
 app.use(
   session({ secret: "keyboard cat", resave: false, saveUninitialized: false })
@@ -23,15 +27,21 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Server is Running Perfectly");
-});
+// app.get("/", (req, res) => {
+//   res.send("Server is Running Perfectly");
+// });
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/explore", exploreRoutes);
 
-app.listen(5000, () => {
-  console.log("Server is running on Port 5000");
+app.use(express.static(path.join(buildPath, "/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(buildPath, "dist", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on Port ${PORT}`);
   connectDB();
 });
