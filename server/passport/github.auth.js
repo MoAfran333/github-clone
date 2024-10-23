affee6e5fe093e989064905552d62e5a9099d167
MoAfran333 <moafran123@gmail.com>
2024-10-23 17:23:02 +0530

    Changed the allback url for the passport.js

--- server/passport/github.auth.js
+++ server/passport/github.auth.js
@@ -22,7 +22,8 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: "/api/auth/github/callback",
+      callbackURL:
+        "https://github-clone-m1qg.onrender.com/api/auth/github/callback",
     },
     async function (accessToken, refreshToken, profile, done) {
       console.log(profile);
